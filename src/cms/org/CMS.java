d0ba3778572921824154b30f8d2e2eb802b7996e
Lee Hicks <lee.lorenzo@gmail.com>
2011-04-11 00:30:22 -0500

    Added About Us button and view

--- src/cms/org/CMS.java
+++ src/cms/org/CMS.java
@@ -2,13 +2,35 @@ package cms.org;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class CMS extends Activity {
+
+public class CMS extends Activity  implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        System.out.println("hell0");
+    
+    
+    View aboutButton = findViewById(R.id.btnAbout);
+    aboutButton.setOnClickListener(this);
+    
     }
+    
+    public void onClick(View v)
+    {
+    	switch(v.getId())
+    	{
+    	
+    	case R.id.btnAbout:
+    		Intent i = new Intent(this, about.class);
+    		startActivity(i);
+    		break;
+    	}    
+    }
+    
+
 }
---
+++ src/cms/org/about.java
@@ -0,0 +1,15 @@
+package cms.org;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class about extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+
+}
