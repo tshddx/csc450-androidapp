32a5c914569ccd4b639dd05d550e0bd442aa4af1
Lee Hicks <lee.lorenzo@gmail.com>
2011-04-11 14:47:53 -0500

    Added info view, not working

--- src/cms/org/CMS.java
+++ src/cms/org/CMS.java
@@ -12,11 +12,17 @@ public class CMS extends Activity  implements OnClickListener{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    
+        setContentView(R.layout.main);    
     
     View aboutButton = findViewById(R.id.btnAbout);
+    View btButton = findViewById(R.id.btnBT); 
+    View webButton = findViewById(R.id.btnWeb);
+    View infoButton = findViewById(R.id.btnAbout);
+    
     aboutButton.setOnClickListener(this);
+    btButton.setOnClickListener(this);
+    webButton.setOnClickListener(this);
+    infoButton.setOnClickListener(this);
     
     }
     
@@ -24,11 +30,23 @@ public class CMS extends Activity  implements OnClickListener{
     {
     	switch(v.getId())
     	{
+    		case R.id.btnAbout:
+    			Intent iAbout = new Intent(this, about.class);
+    			startActivity(iAbout);
+    			break;
+    			
+    		case R.id.btnInfo:
+    			Intent iInfo = new Intent(this, info.class);
+    			startActivity(iInfo);
+    			break;
+    			
+    		case R.id.btnBT:
+    			break;
     	
-    	case R.id.btnAbout:
-    		Intent i = new Intent(this, about.class);
-    		startActivity(i);
-    		break;
+    		case R.id.btnWeb:
+    			break;
+    		
+    		
     	}    
     }
     
---
+++ src/cms/org/info.java
@@ -0,0 +1,15 @@
+package cms.org;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class info extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.info);
+	}
+
+}
