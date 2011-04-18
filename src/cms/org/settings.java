7bbc711e53071efc4b7fcf2c620c24b0d7a4088a
Lee Hicks <lee.lorenzo@gmail.com>
2011-04-18 14:54:07 -0500

    Settings has username and password

--- src/cms/org/settings.java
+++ src/cms/org/settings.java
@@ -1,14 +1,14 @@
 package cms.org;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class settings extends Activity
+public class settings extends PreferenceActivity
 {
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.settings);
+	    addPreferencesFromResource(R.xml.settings);
 	}
 }
