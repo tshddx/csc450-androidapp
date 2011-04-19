02871a4980ab2d650d0ecf67ca39d983c44a54b3
dhockeyavs19 <dhockeyavs19@gmail.com>
2011-04-19 12:40:37 -0500

    added some error checking

--- src/cms/org/fillup.java
+++ src/cms/org/fillup.java
@@ -1,13 +1,28 @@
 package cms.org;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -32,7 +47,7 @@ public class fillup extends Activity
 				// TODO Auto-generated method stub
 				String baseurl = "http://cars.tshaddox.com/api/fillup";
 				String username = "?username=admin";
-				String password = "&password=password";
+				String password = "&password=passwo";
 				String vin = "&vin=qwerty";
 				String odometer= "&odometer=";
 				String gallons ="&gallons=";
@@ -41,38 +56,57 @@ public class fillup extends Activity
 				odometer += etOdometer.getText().toString();
 				gallons += etGallons.getText().toString();
 				
-				fullurl = fullurl += baseurl + username + password + vin + odometer + gallons;
+				if(odometer.length() != 0 && gallons.length() != 0)
+				{
+		
+					fullurl = baseurl + username + password + vin + odometer + gallons;
 				
-				System.out.println(fullurl.toString());
+					System.out.println(fullurl.toString());
 				
-				try{
+					try{
 					
 				
-				URL url = new URL(fullurl);
-				URLConnection connection = url.openConnection();
+						HttpClient client = new DefaultHttpClient();
+						HttpGet getmethod = new HttpGet(fullurl);
+						HttpResponse response = client.execute(getmethod);
 				
-				BufferedReader in = new BufferedReader(
-		                new InputStreamReader(
-		                connection.getInputStream()));
+		
+						BufferedReader in = new BufferedReader( new InputStreamReader(response.getEntity().getContent()));
+						StringBuilder str = new StringBuilder();
+						String line="";
 				
-				String inputLine="";
 
-				while ((in.readLine()) != null)
-				{
-					inputLine += in.readLine();
-				}
+						while ((line = in.readLine()) != null)
+						{
+							str.append(line);
+						}
+				
+						System.out.println("Returned = " + str.toString());
 					
-				in.close();
-				
-				
-
-				TextView t = (TextView) findViewById(R.id.output);
-				t.setText(inputLine);
-				}
-				catch(Exception ex)
-				{
-					System.out.println(ex.getMessage());
-				}
+						in.close();
+						
+//						Context mContext = this;
+//						AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+//						builder.setMessage(str.toString())
+//						       .setCancelable(false)
+//						       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//						           public void onClick(DialogInterface dialog, int id) {
+//						                dialog.cancel();
+//						           }
+//						       });
+//						builder.show();
+						
+						
+						TextView t = (TextView) findViewById(R.id.output);
+						t.setText(str.toString());
+						
+					}
+					catch(Exception ex)
+					{
+						System.out.println("error " + ex.getMessage());
+					}
+					
+				}//end if
 				
 				
 			}
